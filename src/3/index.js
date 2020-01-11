64d67ec259aa5c73897403a8da547b1730d4a5e4
Ivan Ganev <iganevru@gmail.com>
2020-01-12 00:13:05 +0300

    class Stack

--- src/3/index.js
+++ src/3/index.js
@@ -4,24 +4,31 @@
 
 class Stack {
   constructor() {
-    this._storage = {};
+    this._storage = [];
   }
-  /*
+  /**
    * Adds a new value at the end of the stack
    * @param {*} value the value to push
    */
-  push() {}
+  push(value) {
+    this._storage.push(value);
+  }
 
-  /*
+  /**
    * Removes the value at the end of the stack and returns it
    * @return {*} the last and newest value in the stack
    */
-  pop() {}
-  /*
+  pop() {
+    return this._storage.pop();
+  }
+
+  /**
    * Returns the value at the end of the stack without removing it
    * @return {*} the last and newest value in the stack
    */
-  peek() {}
+  peek() {
+    return this._storage[this._storage.length - 1];
+  }
 }
 
-module.exports({ Stack });
+module.exports = { Stack };
--- src/3/index.test.js
+++ src/3/index.test.js
@@ -1,5 +1,20 @@
 const { Stack } = require('./index');
 
-test('Stack test', () => {
-  expect(Stack()).toBe();
+test('Stack tests', () => {
+  let test = new Stack();
+
+  expect(test._storage).toEqual([]);
+
+  test.push('some 1');
+  test.push('some 2');
+  test.push('some 3');
+  expect(test._storage).toEqual(['some 1', 'some 2', 'some 3']);
+
+  const afterPop = test.pop();
+  expect(test._storage).toEqual(['some 1', 'some 2']);
+  expect(afterPop).toEqual('some 3');
+
+  const afterPeek = test.peek();
+  expect(test._storage).toEqual(['some 1', 'some 2']);
+  expect(afterPeek).toEqual('some 2');
 });
