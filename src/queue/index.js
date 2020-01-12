ffe4bb69d6725a8355b5fc415dec7fbb5d6954b1
Ivan Ganev <iganevru@gmail.com>
2020-01-12 05:33:04 +0300

    new format

--- src/queue/index.js
+++ src/queue/index.js
@@ -1,50 +1,5 @@
 'use strict';
 
-/** Class representing a Stack. */
-class Stack {
-  constructor() {
-    this._storage = {};
-    this._length = 0;
-  }
-  /**
-   * Adds a new value at the end of the stack
-   * @param {*} value the value to push
-   */
-  push(value) {
-    if (value) {
-      this._storage[this._length] = value;
-      this._length = this._length + 1;
-    }
-  }
-
-  /**
-   * Removes the value at the end of the stack and returns it
-   * @return {*} the last and newest value in the stack
-   */
-  pop() {
-    if (this._length > 0) {
-      const last = this._storage[this._length - 1];
-      delete this._storage[this._length - 1];
-      this._length = this._length - 1;
-      return last;
-    } else {
-      return undefined;
-    }
-  }
-
-  /**
-   * Returns the value at the end of the stack without removing it
-   * @return {*} the last and newest value in the stack
-   */
-  peek() {
-    if (this._length > 0) {
-      return this._storage[this._length - 1];
-    } else {
-      return undefined;
-    }
-  }
-}
-
 /** Class representing a Queue.
  * @constructor
  */
@@ -95,4 +50,4 @@ class Queue {
   }
 }
 
-module.exports = { Stack, Queue };
+module.exports = { Queue };
--- src/queue/index.test.js
+++ src/queue/index.test.js
@@ -1,4 +1,4 @@
-const { Stack, Queue } = require('./index');
+const { Queue } = require('./index');
 
 describe('Queue Class', () => {
   test('Queue() object ', () => {
@@ -56,57 +56,5 @@ describe('Queue Class', () => {
     afterPeek = test.peek();
     expect(afterPeek).toEqual('some 0');
     expect(test._storage).toEqual({ 0: 'some 0', 1: 'some 1', 2: 'some 2' });
-  });
-});
-
-describe('Stack Class', () => {
-  test('Stack() object ', () => {
-    let test = new Stack();
-    expect(test._storage).toEqual({});
-  });
-
-  test('.push()', () => {
-    let test = new Stack();
-
-    test.push('some 0');
-    test.push('some 1');
-    test.push('some 2');
-    expect(test).toEqual({
-      _storage: { 0: 'some 0', 1: 'some 1', 2: 'some 2' },
-      _length: 3,
-    });
-  });
-
-  test('.pop()', () => {
-    let test = new Stack();
-
-    expect(test.pop()).toBeUndefined();
-
-    test.push('some 0');
-    test.push('some 1');
-    test.push('some 2');
-
-    const afterPop = test.pop();
-    expect(test).toEqual({
-      _storage: { 0: 'some 0', 1: 'some 1' },
-      _length: 2,
-    });
-    expect(afterPop).toEqual('some 2');
-  });
-
-  test('.peek()', () => {
-    let test = new Stack();
-
-    expect(test.peek()).toBeUndefined();
-
-    test.push('some 0');
-    test.push('some 1');
-
-    const afterPeek = test.peek();
-    expect(test).toEqual({
-      _storage: { 0: 'some 0', 1: 'some 1' },
-      _length: 2,
-    });
-    expect(afterPeek).toEqual('some 1');
   });
 });
